100ecffae2e5936e86f521319baa2a582671ba38
Clayton Pereira de Oliveira <pereira.cleiton.oliver@gmail.com>
2018-12-31 20:23:35 -0200

    Tela de login

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php 
+session_start();
 
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
+use \Hcode\Page;
+use \Hcode\PageAdmin;
+use \Hcode\Model\user;
 
-use Hcode\Page;
-use Hcode\PageAdmin;
+
 
 $app = new Slim();
 
@@ -21,12 +24,43 @@ $app->get('/', function() {
 });
 
 $app->get('/admin', function() {
+	User::verifyLogin();
 	$page = new PageAdmin();
 
 	$page->setTpl("index");
  
   });
 
+$app->get("/admin/login",function(){
+	
+	$page = new PageAdmin(array("header" => false,"footer" => false));
+
+	$page->setTpl("login");
+
+
+});
+
+$app->post("/admin/login",function(){
+
+user::login($_POST["login"],$_POST["password"]);
+
+header("Location: /admin");
+exit;
+
+
+});
+$app->get("/admin/logout" , function(){
+
+user::logout();
+
+header("Location:/admin/login");
+exit;
+
+
+});
+
 $app->run();
+
+
 
  ?>
---
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace  Hcode\Model;
+
+use \Hcode\DB\Sql;
+use \Hcode\Model;
+
+
+class User extends Model {
+
+	const SESSION = "user";
+
+	public static function login($login,$password)
+	{
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :LOGIN",array(
+
+		 ":LOGIN" => $login	
+		));
+
+    if(count($results) === 0)
+
+    {
+       throw new \Exception("Usuario inexistente ou senha inválida.");
+       
+
+    }
+
+
+    $data = $results[0];
+
+   if(password_verify($password,$data["despassword"]) === true)
+
+{
+
+ $user = new user();
+
+ $user->setData($data);
+
+
+
+ $_SESSION[user::SESSION] = $user->getValues();
+
+
+ return $user;
+
+ exit;
+
+
+
+}else{
+
+
+	throw new \Exception("Usuario inexistente ou senha inválida.");
+
+
+          }
+
+   	}
+  
+  public static function verifyLogin($inadmin = true)
+  {
+
+  	if(
+  		!isset($_SESSION[User::SESSION])
+  		||
+  		!$_SESSION[User::SESSION]
+  		||
+  		!(int)$_SESSION[user::SESSION]["iduser"] > 0
+  		||
+  		(bool)$_SESSION[user::SESSION]["inadmin"] !== $inadmin
+
+     ){
+     	header("Location: /admin/login");
+
+     	exit;
+	}
+
+
+
+  }
+
+public static function logout()
+{
+
+$_SESSION[User::SESSION] = NULL;
+
+}
+
+
+
+}
+
+
+
+
+
+
+
+?>
